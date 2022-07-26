722a0c2516b5d57854563e6547340639023a85e1
Jannery Briceño <jannerymai@gmail.com>
2022-07-25 19:18:13 -0500

    Añadir estilo a POsts y nombre a posts

--- src/views/wall.js
+++ src/views/wall.js
@@ -51,19 +51,29 @@ export const wall = () => {
   // greeting.innerHTML = `¡Hola, ${localStorage.getItem('nameUser')}!`;
   // console.log(auth.currentUser.email);
   // getName();
-  function increaseLikes() {
+  function handlingLikes(active) {
     let counter = 0;
-    counter++;
+    if (active === true) {
+      counter++;
+    } else {
+      counter--;
+    }
     return counter;
   }
 
-  function createDivs(post) {
+  function createDivs(post, name) {
+    const containerName = document.createElement('div');
     const containerPost = document.createElement('div');
     const containerLikes = document.createElement('div');
     const imgLikes = document.createElement('img');
     const countLikes = document.createElement('p');
+    containerName.innerHTML = name;
     containerPost.innerHTML = post;
+    // containerPost.appendChild(containerName);
+    publishedPostsContainer.appendChild(containerName);
     publishedPostsContainer.appendChild(containerPost);
+    containerName.setAttribute('class', 'container-post-name');
+    containerPost.setAttribute('class', 'container-post');
     // para los likes
     imgLikes.setAttribute('src', 'pictures/heart.png');
     imgLikes.setAttribute('class', 'published-posts-likes-img');
@@ -73,8 +83,16 @@ export const wall = () => {
     containerLikes.appendChild(countLikes);
     publishedPostsContainer.appendChild(containerLikes);
     imgLikes.addEventListener('click', () => {
-      countLikes.innerHTML = increaseLikes();
+      imgLikes.id = 'on';
+      if (imgLikes.id === 'on') {
+        countLikes.innerHTML = handlingLikes(true);
+        imgLikes.id = 'off';
+      } else if (imgLikes.id === 'off') {
+        countLikes.innerHTML = handlingLikes(false);
+        imgLikes.id = 'on';
+      }
       // dar y quitar likes usar toggle?
+      // countLikes.toggleAttribute(countLikes);
     });
   }
 
@@ -86,7 +104,7 @@ export const wall = () => {
         onSnapshot.docs.forEach((document) => {
           //posts.push({ ...doc.data(), id: doc.id });
           posts.push({ name: document.data().name, post: document.data().comment });
-          createDivs(document.data().comment);
+          createDivs(document.data().comment, document.data().name);
         });
       });
   }
@@ -95,7 +113,7 @@ export const wall = () => {
 
   btnPostComment.addEventListener('click', () => {
     if (commentPost.value !== '') {
-      createDivs(commentPost.value);
+      createDivs(commentPost.value, auth.currentUser.displayName);
       saveComment(commentPost.value, auth.currentUser.displayName);
 
       commentPost.value = '';
